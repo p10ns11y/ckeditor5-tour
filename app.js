b1a554066626d8c954ddb2b6a0f4150a1adfac29
p10ns11y <sathyam.peram@gmail.com>
2018-01-03 20:35:19 +0100

    Add inline editor

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import InlineEditor from '@ckeditor/ckeditor5-editor-inline/src/inlineeditor';
+
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 
 ClassicEditor
-    .create( document.querySelector( '#editor' ), {
+    .create( document.querySelector( '#classicEditor' ), {
+        plugins: [ Essentials, Paragraph, Bold, Italic ],
+        toolbar: [ 'bold', 'italic' ]
+    } )
+    .then( editor => {
+        console.log( 'Editor was initialized', editor );
+    } )
+    .catch( error => {
+        console.error( error.stack );
+    } );
+
+InlineEditor
+    .create( document.querySelector( '#inlineEditor' ), {
         plugins: [ Essentials, Paragraph, Bold, Italic ],
         toolbar: [ 'bold', 'italic' ]
     } )
